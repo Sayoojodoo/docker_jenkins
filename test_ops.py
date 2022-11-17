240ba3b13e05b0bf670231ba7441c040d5522515
CybroOdooDev <cybroodoo@gmail.com>
2022-11-17 16:58:14 +0530

    [ADD] file name change

--- test_ops.py
+++ test_ops.py
@@ -3,8 +3,8 @@ from ops import *
 def test_add():
 	assert add(2,3) == 5
 
-def test_substract():
-	assert substract(2,3) == -1
+def test_subtract():
+	assert subtract(2,3) == -1
 
 def test_multiply():
 	assert multiply(2,3) == 6
