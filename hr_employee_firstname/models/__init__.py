7baea04ef3d32977426ef7dfd347cf39f2142e57
rr37 <73212827+Sayoojodoo@users.noreply.github.com>
2022-11-18 13:37:04 +0530

    Update __init__.py

--- hr_employee_firstname/models/__init__.py
+++ hr_employee_firstname/models/__init__.py
@@ -1,3 +1,3 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import hr_employee_base
-from . import hr_employee
+from odoo.addons import hr_employee_base
+from odoo.addons import hr_employee
